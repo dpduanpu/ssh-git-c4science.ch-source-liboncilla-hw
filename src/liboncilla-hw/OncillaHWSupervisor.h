aef56265a7efbc25f962955c27f7bb1558e3a5a4
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-05-01 10:36:22 +0200

    Added installation rules and build skeleton

---
+++ src/liboncilla-hw/OncillaHWSupervisor.h
@@ -0,0 +1,24 @@
+/**
+ * \file OncillaHWSupervisor.h
+ *
+ *  \date May 1, 2012
+ *  \author Alexandre Tuleu
+ */
+
+#ifndef LIBONCILLA_HW_ONCILLAHWSUPERVISOR_H_
+#define LIBONCILLA_HW_ONCILLAHWSUPERVISOR_H_
+
+/**
+ *
+ */
+
+/**
+ *
+ */
+class OncillaHWSupervisor {
+public:
+	OncillaHWSupervisor();
+	virtual ~OncillaHWSupervisor();
+};
+
+#endif // LIBONCILLA_HW_ONCILLAHWSUPERVISOR_H_
