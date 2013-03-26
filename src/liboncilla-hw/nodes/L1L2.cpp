c0e1b9a21cc8fedb8e51d217487977a31268a00f
Arne Nordmann <anordman@cor-lab.uni-bielefeld.de>
2013-03-26 20:44:22 +0100

    Implements DeepCopyable interface for L1L2

--- src/liboncilla-hw/nodes/L1L2.cpp
+++ src/liboncilla-hw/nodes/L1L2.cpp
@@ -7,35 +7,50 @@ namespace hw {
 
 using namespace rci;
 
-L1L2::L1L2(rci::oncilla::Synchronizer & s, const std::string & name) :
-        rci::oncilla::L1L2(s, name) {
+L1L2::L1L2(rci::oncilla::Synchronizer & s , const std::string & name)
+		: rci::oncilla::L1L2(s, name){
 }
 
-L1L2::~L1L2() {
+L1L2::~L1L2(){
 }
 
-void L1L2::setControlMode(ControlModePtr controlMode) {
-    NOT_IMPLEMENTED_STUB(L1L2, setControlMode);
+void L1L2::setControlMode(ControlModePtr controlMode){
+	NOT_IMPLEMENTED_STUB(L1L2, setControlMode);
 }
 
-bool L1L2::setJointImpedance(JointImpedancePtr) {
-    NOT_IMPLEMENTED_STUB(L1L2, setJointImpedance);
+bool L1L2::setJointImpedance(JointImpedancePtr){
+	NOT_IMPLEMENTED_STUB(L1L2, setJointImpedance);
 }
 
-bool L1L2::setJointPosition(JointAnglesPtr position) {
-    NOT_IMPLEMENTED_STUB(L1L2, setJointPosition);
+bool L1L2::setJointPosition(JointAnglesPtr position){
+	NOT_IMPLEMENTED_STUB(L1L2, setJointPosition);
 }
 
-bool L1L2::setJointVelocity(JointVelocitiesPtr velocity) {
-    NOT_IMPLEMENTED_STUB(L1L2, setJointVelocity);
+bool L1L2::setJointVelocity(JointVelocitiesPtr velocity){
+	NOT_IMPLEMENTED_STUB(L1L2, setJointVelocity);
 }
 
-bool L1L2::setJointTorque(JointTorquesPtr position) {
-    NOT_IMPLEMENTED_STUB(L1L2, setJointTorque);
+bool L1L2::setJointTorque(JointTorquesPtr position){
+	NOT_IMPLEMENTED_STUB(L1L2, setJointTorque);
 }
 
-JointAnglesPtr L1L2::getJointPosition() const {
-    NOT_IMPLEMENTED_STUB(L1L2, getJointPosition);
+JointAnglesPtr L1L2::getJointPosition() const{
+	NOT_IMPLEMENTED_STUB(L1L2, getJointPosition);
+}
+
+void L1L2::deepCopyResources(){
+	_lastCommandedPosition = JointAngles::copy(*_lastCommandedPosition);
+
+	_latestJointPosition = JointAngles::copy(*_latestJointPosition);
+
+	_lastCommandedVelocity = JointVelocitiesPtr(
+	        new JointVelocities(_lastCommandedVelocity->_values));
+
+	_lastCommandedTorque = JointTorquesPtr(
+	        new JointTorques(_lastCommandedTorque->_values));
+
+	_lastCommandedImpedance = JointImpedancePtr(
+	        new JointImpedance(_lastCommandedImpedance->_values));
 }
 
 } /* namespace hw */
--- src/liboncilla-hw/nodes/L1L2.h
+++ src/liboncilla-hw/nodes/L1L2.h
@@ -1,25 +1,29 @@
 #pragma once
 
-#include <liboncilla/nodes/L1L2L2.h>
+#include <liboncilla/nodes/L1L2.h>
+
+#include "liboncilla-hw/nodes/DeepCopyable.h"
 
 namespace liboncilla {
 namespace hw {
 
 using namespace rci;
 
-class L1L2: public rci::oncilla::L1L2 {
+class L1L2 : public rci::oncilla::L1L2, public DeepCopyable {
 public:
-    L1L2(rci::oncilla::Synchronizer & s, const std::string & name);
-    virtual ~L1L2();
+	L1L2(rci::oncilla::Synchronizer & s , const std::string & name);
+	virtual ~L1L2();
 
-    virtual void setControlMode(ControlModePtr controlMode);
+	virtual void setControlMode(ControlModePtr controlMode);
 
-    virtual bool setJointImpedance(JointImpedancePtr);
-    virtual bool setJointPosition(JointAnglesPtr position);
-    virtual bool setJointVelocity(JointVelocitiesPtr velocity);
-    virtual bool setJointTorque(JointTorquesPtr position);
+	virtual bool setJointImpedance(JointImpedancePtr);
+	virtual bool setJointPosition(JointAnglesPtr position);
+	virtual bool setJointVelocity(JointVelocitiesPtr velocity);
+	virtual bool setJointTorque(JointTorquesPtr position);
 
-    virtual JointAnglesPtr getJointPosition() const;
+	virtual JointAnglesPtr getJointPosition() const;
+
+	virtual void deepCopyResources();
 };
 
 } /* namespace hw */
