790fb69f129611aca6d3edeaed8f9a5f387a60b2
Arne Nordmann <anordman@cor-lab.uni-bielefeld.de>
2013-03-26 20:00:36 +0100

    Weird atuleu style

--- src/liboncilla-hw/nodes/DeepCopyable.cpp
+++ src/liboncilla-hw/nodes/DeepCopyable.cpp
@@ -5,28 +5,28 @@ namespace hw {
 
 std::set<DeepCopyable*> DeepCopyable::s_changedDeepCopyables;
 
-DeepCopyable::DeepCopyable() {
+DeepCopyable::DeepCopyable(){
 }
 
-DeepCopyable::~DeepCopyable() {
-    // Remove myself from the set before destruction
-    s_changedDeepCopyables.erase(this);
+DeepCopyable::~DeepCopyable(){
+	// Remove myself from the set before destruction
+	s_changedDeepCopyables.erase(this);
 }
 
-void DeepCopyable::mark() {
-    // Add myself to the set
-    s_changedDeepCopyables.insert(this);
+void DeepCopyable::mark(){
+	// Add myself to the set
+	s_changedDeepCopyables.insert(this);
 }
 
-void DeepCopyable::makeDeepCopies() {
-    for (std::set<DeepCopyable*>::iterator i = s_changedDeepCopyables.begin();
-            i != s_changedDeepCopyables.end(); ++i) {
-        (*i)->deepCopyResources();
-    }
+void DeepCopyable::makeDeepCopies(){
+	for (std::set<DeepCopyable*>::iterator i = s_changedDeepCopyables.begin();
+	        i != s_changedDeepCopyables.end(); ++i) {
+		(*i)->deepCopyResources();
+	}
 }
 
-void DeepCopyable::clearMarks() {
-    s_changedDeepCopyables.clear();
+void DeepCopyable::clearMarks(){
+	s_changedDeepCopyables.clear();
 }
 
 }
--- src/liboncilla-hw/nodes/DeepCopyable.h
+++ src/liboncilla-hw/nodes/DeepCopyable.h
@@ -12,37 +12,37 @@ namespace hw {
  */
 class DeepCopyable {
 public:
-    DeepCopyable();
-    virtual ~DeepCopyable();
+	DeepCopyable();
+	virtual ~DeepCopyable();
 
-    /**
-     * Tell the node to deep-copy its resources. This one has to be implemented
-     * by the ResourceNode implementing this interface. It has to make a deep
-     * copy of all contained resources (JointAngles, Forces, etc., usually
-     * shared pointers).
-     */
-    virtual void deepCopyResources() = 0;
+	/**
+	 * Tell the node to deep-copy its resources. This one has to be implemented
+	 * by the ResourceNode implementing this interface. It has to make a deep
+	 * copy of all contained resources (JointAngles, Forces, etc., usually
+	 * shared pointers).
+	 */
+	virtual void deepCopyResources() = 0;
 
-    /**
-     * Mark this node as changed by adding it to ChangedDeepCopyables.
-     */
-    void mark();
+	/**
+	 * Mark this node as changed by adding it to ChangedDeepCopyables.
+	 */
+	void mark();
 
-    /**
-     * Make deep copies of all items in the ChangedDeepCopyables set.
-     */
-    static void makeDeepCopies();
+	/**
+	 * Make deep copies of all items in the ChangedDeepCopyables set.
+	 */
+	static void makeDeepCopies();
 
-    /**
-     * Clear the list of to be deep copied objects, ChangedDeepCopyables.
-     */
-    static void clearMarks();
+	/**
+	 * Clear the list of to be deep copied objects, ChangedDeepCopyables.
+	 */
+	static void clearMarks();
 
 private:
-    /**
-     * Set of DeepCopyable items, that are to be deep copied.
-     */
-    static std::set<DeepCopyable*> s_changedDeepCopyables;
+	/**
+	 * Set of DeepCopyable items, that are to be deep copied.
+	 */
+	static std::set<DeepCopyable*> s_changedDeepCopyables;
 
 };
 
