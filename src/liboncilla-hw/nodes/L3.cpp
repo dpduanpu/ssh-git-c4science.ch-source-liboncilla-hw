6d5d7c5dc6bdb5e6726ffb25bdbeefd9aa959d7f
Arne Nordmann <anordman@cor-lab.uni-bielefeld.de>
2013-03-26 20:49:22 +0100

    Implements DeepCopyable interface for L3

--- src/liboncilla-hw/nodes/L3.cpp
+++ src/liboncilla-hw/nodes/L3.cpp
@@ -5,15 +5,19 @@
 namespace liboncilla {
 namespace hw {
 
-L3::L3(const std::string & name) :
-        rci::oncilla::L3(name) {
+L3::L3(const std::string & name)
+		: rci::oncilla::L3(name){
 }
 
-L3::~L3() {
+L3::~L3(){
 }
 
-JointAnglesPtr L3::getJointPosition() const {
-    NOT_IMPLEMENTED_STUB(L3, getJointPosition);
+JointAnglesPtr L3::getJointPosition() const{
+	NOT_IMPLEMENTED_STUB(L3, getJointPosition);
+}
+
+void L3::deepCopyResources(){
+	_latestJointPosition = JointAngles::copy(*_latestJointPosition);
 }
 
 } /* namespace hw */
--- src/liboncilla-hw/nodes/L3.h
+++ src/liboncilla-hw/nodes/L3.h
@@ -2,19 +2,23 @@
 
 #include <liboncilla/nodes/L3.h>
 
-#include <liboncilla-hw/Common.h>
+#include "liboncilla-hw/Common.h"
+#include "liboncilla-hw/nodes/DeepCopyable.h"
 
 namespace liboncilla {
 namespace hw {
 
 using namespace rci;
 
-class L3: public rci::oncilla::L3 {
+class L3 : public rci::oncilla::L3 ,
+           public DeepCopyable {
 public:
-    L3(const std::string & name);
-    virtual ~L3();
+	L3(const std::string & name);
+	virtual ~L3();
 
-    virtual JointAnglesPtr getJointPosition() const;
+	virtual JointAnglesPtr getJointPosition() const;
+
+	virtual void deepCopyResources();
 
 };
 
