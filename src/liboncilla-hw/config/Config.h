faa52572ca231a7ab402f36d47f795c5d2f20bf0
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-07-04 16:30:22 +0200

    Refactors Motor config section for simplicity

--- src/liboncilla-hw/config/Config.h
+++ src/liboncilla-hw/config/Config.h
@@ -7,7 +7,9 @@
 
 #pragma once
 
-#include "MainSection.h"
+#include <liboncilla-hw/config/MainSection.h>
+#include <liboncilla-hw/config/MotorSection.h>
+
 
 namespace liboncilla {
 namespace hw {
@@ -18,6 +20,9 @@ public:
 	virtual ~Config();
 
 	BCPP_ADD_SUBSECTION(MainSection,Main,"main","Main settings for liboncilla-hw")
+	BCPP_ADD_SUBSECTION(MotorSection,Motor,"motor","Section for motor configuration")
+
+
 };
 
 } /* namespace hw */
