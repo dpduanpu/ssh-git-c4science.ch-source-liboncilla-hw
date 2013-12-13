b6dc4938eb380ed4dec67ee082f1af4ef746e200
Alexandre Tuleu <alexandre.tuleu.2005@polyetchnique.org>
2013-12-13 08:14:50 +0100

    Removes comentted debug output and wrapped remaining one
    Debugging to std::cout is somehow a bad practice. Even in a rush,
    please consider some helper function, like the log() macro from
    biorob-cpp. This macro is really powerful, and in case you compile
    your code in cmake Release mode, will automatically removes this
    unuseful output.

--- src/liboncilla-hw/synchronization/SBCPQueue.cpp
+++ src/liboncilla-hw/synchronization/SBCPQueue.cpp
@@ -7,6 +7,8 @@
 
 #include "SBCPQueue.h"
 
+#include <biorob-cpp/log/Logger.h>
+
 #include <libsbcp/utils/Config.h>
 
 #include <liboncilla-hw/config/Config.h>
@@ -17,14 +19,12 @@ namespace hw {
 SBCPQueue::SBCPQueue(const Config & config) 
 	: Queue(config.Main().SBCPPriority(), true)
 	, d_bus() {
-    //std::cout << "SBCPQueue::SBCPQueue()" << std::endl;
 	sbcp::Config sbcpConfig;
 	sbcpConfig.LoadAllFiles();
 
 	// create links to the 4 motorboards
-	unsigned int requiredFrameSize = 4; //(=4 consecutive packets send at oncefrom roboard to master communication board)
-	// create links to the 4 motorboards
-	unsigned int requiredPacketSize = 32; //(Size of the biggest packet you will send, the actual maximum might be higher)
+	unsigned int requiredFrameSize = 4;   // 4 consecutive packets send at oncefrom roboard to master communication board
+	unsigned int requiredPacketSize = 32; // Size of the biggest packet you will send, the actual maximum might be higher
 
 	d_bus = sbcpConfig.OpenDefaultBusWithFrame(requiredFrameSize,
 	                                           requiredPacketSize);
@@ -32,7 +32,7 @@ SBCPQueue::SBCPQueue(const Config & config)
 	if (!d_bus) {
 		throw std::runtime_error("SBCPQueue::SBCPQueue() Can't load bus due to missing configuration.");
 	} else {
-		std::cout << "Bus Workflow: " << d_bus->CurrentWorkflow() << std::endl;
+		log(debug, "Bus Workflow: ", d_bus->CurrentWorkflow());
 	}
 
 	const MotorDriverGroup & devices = config.Motors().Devices();
@@ -51,23 +51,17 @@ SBCPQueue::SBCPQueue(const Config & config)
 }
 
 SBCPQueue::~SBCPQueue() {
-    //std::cout << "SBCPQueue::~SBCPQueue()" << std::endl;
 }
 
 void SBCPQueue::DownstreamData() {
-	//std::cout << "------ SBCPQueue::DownstreamData() ------" << std::endl;
 	for (MotorAndEncoderByL1L2::const_iterator motAndEnc = d_motAndEncByL1L2.begin(); 
 	     motAndEnc != d_motAndEncByL1L2.end();
 	     ++motAndEnc) {
 		motAndEnc->second.motor->GoalPosition().Set(motAndEnc->first->nodeToQueueJointPosition());
-		//std::cout << "set position: " << motAndEnc->first->nodeToQueueJointPosition() << std::endl;
-		//std::cout << " set position: " << motAndEnc->second.motor->GoalPosition().Get() << std::endl;
 	}
-	//std::cout << "------ SBCPQueue::DownstreamData() ------" << std::endl << std::endl;
 }
 
 void SBCPQueue::UpstreamData() {
-	//std::cout << "------ SBCPQueue::UpstreamData() ------" << std::endl;
 	for (MotorAndEncoderByL1L2::const_iterator motAndEnc = d_motAndEncByL1L2.begin(); 
 	     motAndEnc != d_motAndEncByL1L2.end();
 	     ++motAndEnc) {
@@ -99,11 +93,9 @@ void SBCPQueue::UpstreamData() {
 			                                     (enc->second->PositionAndStatus().Get() & 0xc000) >> 14);
 		}
 	}
-	//std::cout << "------ SBCPQueue::UpstreamData() ------" << std::endl << std::endl;
 }
 
 void SBCPQueue::PerformIO() {
-    // std::cout << "------ SBCPQueue::PerformIO() ------" << std::endl;
 	sbcp::ScheduledWorkflow & w = this->d_bus->Scheduled();
 
 	// Start transfer
@@ -111,11 +103,9 @@ void SBCPQueue::PerformIO() {
 
 	// Wait for transfer to complete
 	w.WaitForTransfersCompletion();
-	//std::cout << "------ SBCPQueue::PerformIO() ------" << std::endl << std::endl;
 }
 
 void SBCPQueue::InitializeIO() {
-	// std::cout << "------ SBCPQueue::InitializeIO() ------" << std::endl;
 	// Enable scheduled woirkflow
 	sbcp::ScheduledWorkflow & w = this->d_bus->Scheduled();
 
@@ -125,22 +115,18 @@ void SBCPQueue::InitializeIO() {
 	     ++md) {
 
 		if (md->second == NULL) {
-			std::cout << "SBCPQueue::InitializeIO: Ignoring motor driver "
-			          << md->second << std::endl;
+			log(debug, "SBCPQueue::InitializeIO: Ignoring motor driver ",
+			    LegPrefix(md->first) );
 		} else {
-			std::cout << "appending scheduled device " << md->second
-			          << std::endl;
+			log( debug,  "Appending scheduled device " , LegPrefix(md->first) );
 			w.AppendScheduledDevice(std::tr1::static_pointer_cast<sbcp::Device>(md->second));
 		}
 	}
-	//std::cout << "------ SBCPQueue::InitializeIO() ------" << std::endl << std::endl;
 }
 
 void SBCPQueue::DeinitializeIO() {
-	// std::cout << "------ SBCPQueue::DeinitializeIO() ------" << std::endl;
 	// Disable scheduled woirkflow
 	this->d_bus->Lazy();
-	// std::cout << "------ SBCPQueue::DeinitializeIO() ------" << std::endl << std::endl;
 }
 
 void SBCPQueue::RegisterL0(rci::oncilla::Leg l, const L0::Ptr & node) {
@@ -169,8 +155,8 @@ void SBCPQueue::RegisterL1(rci::oncilla::Leg l, const L1L2::Ptr & node) {
 
 	if (fi->second == NULL) {
 		// TODO: Throw?
-		std::cout << "SBCPQueue::RegisterL1: Ignoring L1 of leg "
-		          << LegPrefix(l) << ". Is it connected?" << std::endl;
+		log(debug, "SBCPQueue::RegisterL1: Ignoring L1 of leg ",
+		    LegPrefix(l),". Is it connected?");
 		return;
 	}
 
@@ -200,8 +186,7 @@ void SBCPQueue::RegisterL2(rci::oncilla::Leg l, const L1L2::Ptr & node) {
 
 	if (fi->second == NULL) {
 		// TODO: Throw?
-		std::cout << "SBCPQueue::RegisterL2: Ignoring L2 of leg "
-		          << LegPrefix(l) << ". Is it connected?" << std::endl;
+		log(debug, "SBCPQueue::RegisterL2: Ignoring L2 of leg ", LegPrefix(l), ". Is it connected?");
 		return;
 	}
 
@@ -228,8 +213,8 @@ void SBCPQueue::RegisterL3(rci::oncilla::Leg l, const L3::Ptr & node) {
 
 	if (fi->second == NULL) {
 		// TODO: Throw?
-		std::cout << "SBCPQueue::RegisterL3: Ignoring L3 of leg "
-		          << LegPrefix(l) << ". Is it connected?" << std::endl;
+		log(debug, "SBCPQueue::RegisterL3: Ignoring L3 of leg ",
+		    LegPrefix(l), ". Is it connected?");
 		return;
 	}
 
@@ -240,7 +225,7 @@ sbcp::amarsi::MotorDriver::Ptr
 SBCPQueue::OpenAndConfigureMotorDriver(const MotorDriverSection & def, 
                                        const BrushlessParameterGroup & params,
                                        int16_t expectedTsInMs) {
-    std::cout << "SBCPQueue::OpenAndConfigureMotorDriver()" << std::endl;
+	log(debug, "SBCPQueue::OpenAndConfigureMotorDriver()");
 
     this->d_bus->Lazy();
 
@@ -248,16 +233,16 @@ SBCPQueue::OpenAndConfigureMotorDriver(const MotorDriverSection & def,
 		throw std::runtime_error("SBCPQueue::OpenAndConfigureMotorDriver() Can't"
 		                         " load bus due to missing configuration.");
 	} else {
-		std::cout << "SBCPQueue::OpenAndConfigureMotorDriver() Bus Workflow: "
-		          << d_bus->CurrentWorkflow() << std::endl;
+		log(debug, "SBCPQueue::OpenAndConfigureMotorDriver() Bus Workflow: ",
+		    d_bus->CurrentWorkflow());
 	}
 
 	sbcp::amarsi::MotorDriver::Ptr res = d_bus->OpenDevice< sbcp::amarsi::MotorDriver > (def.BoardID());
 
 	if (!res) {
 		// board is just not here, buit we will throw later if it is required.
-		std::cout << "SBCPQueue::OpenAndConfigureMotorDriver() Ignoring board, "
-		          << "doesn't seem to be connected." << std::endl;
+		log(debug, "SBCPQueue::OpenAndConfigureMotorDriver() Ignoring board, ",
+		    "doesn't seem to be connected.");
 		return res;
 	}
 
@@ -266,7 +251,7 @@ SBCPQueue::OpenAndConfigureMotorDriver(const MotorDriverSection & def,
 	if(! (m1Calibrated || m2Calibrated)){
 		res->CalibrateMotors();
 	} else {
-		std::cout << "Motor already calibrated"<<std::endl;
+		log(debug, "Motor already calibrated");
 	}
 
 	while(!(m1Calibrated && m2Calibrated)){
@@ -274,7 +259,7 @@ SBCPQueue::OpenAndConfigureMotorDriver(const MotorDriverSection & def,
 		m1Calibrated = GetCalibrationStatus(res->Motor1(), 1);
 		m2Calibrated = GetCalibrationStatus(res->Motor2(), 2);
 	}
-	std::cout << "Motor calibrated" << std::endl << std::endl;
+	lohg(debug, "Motor calibrated");
 
 	SetMotorParameters(params, def.M1Params(), expectedTsInMs, res->Motor1());
 	SetMotorParameters(params, def.M2Params(), expectedTsInMs, res->Motor2());
@@ -294,7 +279,7 @@ SBCPQueue::OpenAndConfigureMotorDriver(const MotorDriverSection & def,
 void SBCPQueue::SetMotorParameters(const BrushlessParameterGroup & paramGroup,
                                    const std::string & paramName, int16_t expectedTsInMs,
                                    sbcp::amarsi::MotorDriver::Motor & motor) {
-	std::cout << "SBCPQueue::SetMotorParameters()" << std::endl;
+	log(debug, "SBCPQueue::SetMotorParameters()" );
 	//due to a bad design of biorob-cpp dynamic section we should do this
 	std::tr1::shared_ptr<BrushlessParameterSection> params =
 		const_cast<BrushlessParameterGroup &>(paramGroup).SubSection(paramName);
